bc575fc085d30d1e83701b513296b9f77fe76682
samar mohanty <samarmohanty360@gmail.com>
2023-05-16 19:23:39 +0530

    Hyper tuneing

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ from src.exception import CostumException
 from src.logger import logging
 import dill 
 
+from sklearn.model_selection import GridSearchCV
 from src.exception import CostumException
 from src.logger import logging
 from sklearn.metrics import r2_score
@@ -25,20 +26,33 @@ def save_object(file_path,obj):
 logging.info("file dir created ")
 
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models,param):
     try:
         report = {}
 
-        for model_name, model in models.items():
-            model.fit(X_train, y_train)  # train model
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
 
             y_train_pred = model.predict(X_train)
+
             y_test_pred = model.predict(X_test)
 
             train_model_score = r2_score(y_train, y_train_pred)
+
             test_model_score = r2_score(y_test, y_test_pred)
 
-            report[model_name] = test_model_score
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
 
         return report
     except Exception as e:
